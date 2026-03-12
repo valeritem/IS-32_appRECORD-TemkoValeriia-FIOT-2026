44af2609e0d7fc4cd9fa21317b2799997ccbe89b
Lera <valeri2006tem@gmail.com>
2026-03-12 10:23:04 +0200

    add GitHub Pages site

--- base-path.js
+++ base-path.js
@@ -1,3 +1,3 @@
 export const BASE_PATH =
   process.env.BASE_PATH ??
-  (process.env.NODE_ENV === "production" ? "/Labs-Markdown-Template" : "");
+  (process.env.NODE_ENV === "production" ? "/IS-32_appRECORD-TemkoValeriia-FIOT-2026" : "");
